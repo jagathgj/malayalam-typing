420f77d7c6fb262c2ddea492aaacc8312a4e5d03
Jagath Jayakumar <jagath.gj@gmail.com>
2024-11-17 16:30:59 +0530

    latest template added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import IBMHeader from "./Header";
+
+const App = () => {
+  return (
+    <div className="container">
+      <IBMHeader />
+      <div className="wrapper">
+        <h5>Carbon Template</h5>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
